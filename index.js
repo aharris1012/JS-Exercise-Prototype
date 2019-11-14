3ce5e37b5ea1e96c66c7b5e6aa65fa4facb4d033
Andrea Quintana <andreaquintana@andreas-mbp.lan>
2019-11-14 16:28:27 -0600

    done

--- index.js
+++ index.js
@@ -142,15 +142,29 @@ return `Playing with ${this.favoriteToy}!`
 }
 
 
-/* 
-  TASK 4
+
+  /*TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
-*/
+  
+  1. Principle 1: Window/Global Object Binding
+
+    When in the global scope, the value of “this” will be the window/console Object;
+  
+  2. Principle 2: Implicit Binding
+    
+    Whenever a function is called by a preceding dot, the object before that dot is this
+
+   3. Principle 3: New binding
+
+    Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
+
+   4. Principle 4: Explicit binding
+
+    When JavaScript calls or apply method is used, is specific.
+
+  */
+
 
 
 ///////// END OF CHALLENGE /////////
